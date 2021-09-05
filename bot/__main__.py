3aeee7825403f6250a794f90d5ad0978976937c5
Hafitz Setya <hafitz666@outlook.co.id>
2021-09-05 13:16:31 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,4 @@
-from telegram.ext import CommandHandler, run_async
+from telegram.ext import CommandHandler
 from bot import dispatcher, updater, botStartTime
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
@@ -6,27 +6,23 @@ from .helper.telegram_helper.filters import CustomFilters
 from .modules import authorize, list
 
 
-@run_async
 def start(update, context):
     start_string = '\x1fThis bot can search file from your Google Drive!\x1f'
     sendMessage(start_string, context.bot, update)
 
 
-@run_async
 def log(update, context):
     sendLogFile(context.bot, update)
 
 
-botcmds = [
-        (f'{BotCommands.ListCommand}','Searches files in Drive')
-    ]
+botcmds = [(f'{BotCommands.ListCommand}','Searches files in Drive')]
 
 
 def main():
     bot.set_my_commands(botcmds)
 
-    start_handler = CommandHandler(BotCommands.StartCommand, start, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
-    log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter)
+    start_handler = CommandHandler(BotCommands.StartCommand, start, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter, run_async=True)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(log_handler)
