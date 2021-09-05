43728ee7c75628392319abdf4445d984583c2975
Hafitz Setya <hafitz666@outlook.co.id>
2021-09-05 13:17:18 +0700

    Update filters.py

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -1,22 +1,22 @@
-from telegram.ext import BaseFilter
+from telegram.ext import MessageFilter
 from telegram import Message
 from bot import AUTHORIZED_CHATS, OWNER_ID
 
 class CustomFilters:
-    class _OwnerFilter(BaseFilter):
+    class _OwnerFilter(MessageFilter):
         def filter(self, message):
             return bool(message.from_user.id == OWNER_ID)
 
     owner_filter = _OwnerFilter()
 
-    class _AuthorizedUserFilter(BaseFilter):
+    class _AuthorizedUserFilter(MessageFilter):
         def filter(self, message):
             id = message.from_user.id
             return bool(id in AUTHORIZED_CHATS or id == OWNER_ID)
 
     authorized_user = _AuthorizedUserFilter()
 
-    class _AuthorizedChat(BaseFilter):
+    class _AuthorizedChat(MessageFilter):
         def filter(self, message):
             return bool(message.chat.id in AUTHORIZED_CHATS)
 
